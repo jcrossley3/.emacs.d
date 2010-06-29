0b8ea0ad6041c503698913015d2f91b75675ba89
Jim Crossley <jim@jimbook.crossleys.home>
2010-06-29 11:28:53 -0400

    More dev env familiarities

--- my-env.el
+++ my-env.el
@@ -1,7 +1,41 @@
 
+(if (require 'color-theme nil t)
+    (progn
+      (color-theme-initialize)
+      ;; (color-theme-jsc-dark)
+      ;; (color-theme-dark-blue)
+      ;; (color-theme-subtle-hacker)
+      ;; (color-theme-ld-dark)
+      ;; (color-theme-clarity)
+      (color-theme-hober)))
+
 ;; ensure necessary libs are available on the PATH because MacOS
 ;; doesn't make this intuitive or easy
 (setenv "PATH" (concat (expand-file-name "~/local/maven/bin") ":/opt/local/bin:/opt/local/sbin:" 
 		       (getenv "PATH")))
 
+;;; The way previous input should be matched in a shell
+(defun my-previous-input-behavior ()
+  (local-set-key "\M-p" 'comint-previous-matching-input-from-input))
+(add-hook 'comint-mode-hook 'my-previous-input-behavior)
 
+;;; Convenient shortcuts
+(global-set-key (quote [f9]) (quote svn-status))
+(global-set-key (quote [f8]) (quote browse-url-at-point))
+(global-set-key (quote [f7]) (quote compile))
+(global-set-key (quote [f4]) (quote browse-kill-ring))
+(global-set-key (quote [f3]) (quote grep-find))
+
+;;; Map goto-line to M-g
+;(global-set-key "\M-g" 'goto-line)
+;;; Make <home> be C-a without the whitespace
+(global-set-key [(home)] 'back-to-indentation)
+
+;;; smarter other-window
+(require 'windmove)
+(windmove-default-keybindings)
+
+;;; For calendar lunar/solar stuff
+(setq calendar-latitude 34.0)
+(setq calendar-longitude -84.4)
+(setq calendar-location-name "Roswell, GA")
--- my-ruby.el
+++ my-ruby.el
@@ -6,6 +6,9 @@
   (local-set-key [f1] 'yari))
 (add-hook 'ruby-mode-hook 'ri-bind-key)
 
+;;; apparently inf-ruby doesn't fire the comint-mode-hook
+(add-hook 'inf-ruby-mode-hook 'my-previous-input-behavior)
+
 ;;; Another ri alternative
 ;; (setq ri-ruby-script (concat config-dir "ri-emacs/ri-emacs.rb"))
 ;; (autoload 'ri (concat config-dir "ri-emacs/ri-ruby.el") nil t)
