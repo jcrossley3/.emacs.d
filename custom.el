28ba761bcbf063a12c234e81ed4659967348f42b
Jim Crossley <jim@crossleys.org>
2010-07-06 00:06:36 -0400

    Be colorful in a shell

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(ansi-color-for-comint-mode t)
  '(aquamacs-additional-fontsets nil t)
  '(aquamacs-autoface-mode nil)
  '(aquamacs-customization-version-id 208 t)
